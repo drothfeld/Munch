9c5c7b9b791eeb789f61bdd1a89b988babc6e47a
Dylan Rothfeld <drothfel@gmail.com>
2018-03-12 11:08:00 -0700

    moved core create account functionality to seperate view controller

--- Munch/View Controllers/LoginViewController.swift
+++ Munch/View Controllers/LoginViewController.swift
@@ -51,91 +51,15 @@ class LoginViewController: UIViewController {
     
     // User wants to create a new account
     @IBAction func createNewAccountButtonPressed(_ sender: Any) {
-        // Check if a user with the given username already exists
-        let database = Database.database().reference()
-        database.child("Users").observeSingleEvent(of: DataEventType.value, with: { (snapshot) in
-            print(snapshot)
-            
-            // User already exists
-            if snapshot.hasChild(Auth.auth().currentUser!.uid) {
-                // TODO: IS NOT PROPERLY WORKING
-                self.errorMessageLabel.isHidden = false
-                self.errorMessageLabel.text = "That username is already in use"
-                self.invalidLoginCredentials = true
-            }
-            
-            // Validate username and password fields
-            else {
-                // Checking username
-                if (!self.isValidEmail(username: self.usernameTextField.text!)) {
-                    self.errorMessageLabel.isHidden = false
-                    self.errorMessageLabel.text = "Invalid username/email"
-                    self.invalidLoginCredentials = true
-                }
-                // Checking password
-                else if (!self.isValidPassword(password: self.passwordTextField.text!)) {
-                    self.errorMessageLabel.isHidden = false
-                    self.errorMessageLabel.text = "Invalid password"
-                    self.invalidLoginCredentials = true
-                }
-                // All validation passed, create new account
-                else {
-                    self.errorMessageLabel.isHidden = true
-                    self.invalidLoginCredentials = false
-                    Auth.auth().createUser(withEmail: self.usernameTextField.text!, password: self.passwordTextField.text!) {
-                        (user, error) in
-                        // ...
-                    }
-                    self.performSegue(withIdentifier: "homeLogin", sender: self)
-                }
-            }
-        })
+        
     }
     
     // Checks whether segue should be preformed based on login info
     func shouldPerformSegueWithIdentifier(_ identifier: String!, sender: AnyObject!) -> Bool {
-        NSLog("test")
         if (self.invalidLoginCredentials) {
             return false
         }
         return true
-    }
-    
-    // Checks if a given string is a valid email address
-    func isValidEmail(username: String) -> Bool {
-        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
-        return emailTest.evaluate(with: username)
-    }
-    
-    // Checks if a given string is a valid password
-    func isValidPassword(password: String) -> Bool {
-        // Check if password contains uppercase letter
-        let capitalLetterRegEx  = ".*[A-Z]+.*"
-        let texttest = NSPredicate(format:"SELF MATCHES %@", capitalLetterRegEx)
-        let capitalResult = texttest.evaluate(with: password)
-        
-        // Check if password contains number
-        let numberRegEx  = ".*[0-9]+.*"
-        let texttest1 = NSPredicate(format:"SELF MATCHES %@", numberRegEx)
-        let numberResult = texttest1.evaluate(with: password)
-        
-        // Check if password contains a special character
-        let specialCharacterRegEx  = ".*[!&^%$#@()/]+.*"
-        let texttest2 = NSPredicate(format:"SELF MATCHES %@", specialCharacterRegEx)
-        let specialResult = texttest2.evaluate(with: password)
-        
-        // Check password length
-        let minimumRequiredLength = 6
-        var countResult = false
-        if (password.count < minimumRequiredLength) {
-            countResult = false
-        } else {
-            countResult = true
-        }
-        
-        // All conditions must be met
-        return (capitalResult && numberResult && specialResult && countResult)
     }
     
     // Configuring ui elements when app loads
--- Munch/View Controllers/NewUserViewController.swift
+++ Munch/View Controllers/NewUserViewController.swift
@@ -8,13 +8,19 @@
 
 import Foundation
 import FirebaseAuth
+import FirebaseDatabase
 
 class NewUserViewController: UIViewController {
+    // Controller Elements
+    var invalidLoginCredentials: Bool = true
+    
     // UI Elements
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var newUserButton: UIButton!
-    @IBOutlet weak var errorMessageLabel: UIButton!
+    @IBOutlet weak var errorMessageLabel: UILabel!
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
     
     // Onload
     override func viewDidLoad() {
@@ -28,7 +34,118 @@ class NewUserViewController: UIViewController {
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "login_background.png")!)
         passwordTextField.attributedPlaceholder = NSAttributedString(string:"Password", attributes: [NSAttributedStringKey.foregroundColor: UIColor.white])
         usernameTextField.attributedPlaceholder = NSAttributedString(string:"Username", attributes: [NSAttributedStringKey.foregroundColor: UIColor.white])
+        firstNameTextField.attributedPlaceholder = NSAttributedString(string:"First Name", attributes: [NSAttributedStringKey.foregroundColor: UIColor.white])
+        lastNameTextField.attributedPlaceholder = NSAttributedString(string:"Last Name", attributes: [NSAttributedStringKey.foregroundColor: UIColor.white])
         newUserButton.imageView?.layer.cornerRadius = 10
+    }
+
+    // User tries to create new account
+    @IBAction func createAccountButtonPressed(_ sender: Any) {
+        // Check if a user with the given username already exists
+        let database = Database.database().reference()
+        database.child("Users").observeSingleEvent(of: DataEventType.value, with: { (snapshot) in
+            print(snapshot)
+            
+            // User already exists
+//            if snapshot.hasChild(Auth.auth().currentUser!.uid) {
+            if (5 < 1) {
+                // TODO: IS NOT PROPERLY WORKING
+                self.errorMessageLabel.isHidden = false
+                self.errorMessageLabel.text = "That username is already in use"
+                self.invalidLoginCredentials = true
+            }
+                
+            // Validate create account fields
+            else {
+                // Checking username
+                if (!self.isValidEmail(username: self.usernameTextField.text!)) {
+                    self.errorMessageLabel.isHidden = false
+                    self.errorMessageLabel.text = "Invalid username/email"
+                    self.invalidLoginCredentials = true
+                }
+                    // Checking password
+                else if (!self.isValidPassword(password: self.passwordTextField.text!)) {
+                    self.errorMessageLabel.isHidden = false
+                    self.errorMessageLabel.text = "Invalid password"
+                    self.invalidLoginCredentials = true
+                }
+                    // Checking profile
+                else if (!self.isValidProfile(firstname: self.firstNameTextField.text!, lastname: self.lastNameTextField.text!)) {
+                    self.errorMessageLabel.isHidden = false
+                    self.errorMessageLabel.text = "Invalid first/last name"
+                }
+                    // All validation passed, create new account
+                else {
+                    self.errorMessageLabel.isHidden = true
+                    self.invalidLoginCredentials = false
+                    Auth.auth().createUser(withEmail: self.usernameTextField.text!, password: self.passwordTextField.text!) {
+                        (user, error) in
+                        // ...
+                    }
+                    // Create new JSON user endpoints
+                    
+                    
+                    // If everything went smoothly, take the user back to the login screen
+                    self.performSegue(withIdentifier: "backToLogIn", sender: self)
+                }
+            }
+        })
+    }
+    
+    // Checks if a given string is a valid email address
+    func isValidEmail(username: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: username)
+    }
+    
+    // Checks if a given string is a valid password
+    func isValidPassword(password: String) -> Bool {
+        // Check if password contains uppercase letter
+        let capitalLetterRegEx  = ".*[A-Z]+.*"
+        let texttest = NSPredicate(format:"SELF MATCHES %@", capitalLetterRegEx)
+        let capitalResult = texttest.evaluate(with: password)
+        
+        // Check if password contains number
+        let numberRegEx  = ".*[0-9]+.*"
+        let texttest1 = NSPredicate(format:"SELF MATCHES %@", numberRegEx)
+        let numberResult = texttest1.evaluate(with: password)
+        
+        // Check if password contains a special character
+        let specialCharacterRegEx  = ".*[!&^%$#@()/]+.*"
+        let texttest2 = NSPredicate(format:"SELF MATCHES %@", specialCharacterRegEx)
+        let specialResult = texttest2.evaluate(with: password)
+        
+        // Check password length
+        let minimumRequiredLength = 6
+        var countResult = false
+        if (password.count < minimumRequiredLength) {
+            countResult = false
+        } else {
+            countResult = true
+        }
+        
+        // All conditions must be met
+        return (capitalResult && numberResult && specialResult && countResult)
+    }
+    
+    // Checks if given profile strings are valid
+    func isValidProfile(firstname: String, lastname: String) -> Bool {
+        // Checks if first/last names are at least the minimum length
+        let minimumRequiredLength = 2
+        let firstnameResult: Bool = (firstname.count > minimumRequiredLength)
+        let lastnameResult: Bool = (lastname.count > minimumRequiredLength)
+        
+        // All conditions must be met
+        return (firstnameResult && lastnameResult)
+    }
+    
+    // Checks whether segue should be preformed based on login info
+    func shouldPerformSegueWithIdentifier(_ identifier: String!, sender: AnyObject!) -> Bool {
+        if (self.invalidLoginCredentials) {
+            return false
+        }
+        return true
     }
     
     // Hides status bar
