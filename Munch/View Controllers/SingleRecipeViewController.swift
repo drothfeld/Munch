9bafeab0c21a4a7ec99b06d6ae16e0c62cf469a3
Dylan Rothfeld <drothfel@gmail.com>
2018-04-13 17:22:38 -0700

    new view controller for single recipe view

---
+++ Munch/View Controllers/SingleRecipeViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SingleRecipeViewController.swift
+//  Munch
+//
+//  Created by Dylan Rothfeld on 4/13/18.
+//  Copyright © 2018 Dylan Rothfeld. All rights reserved.
+//
+
+import Foundation
+import FirebaseAuth
+import FirebaseDatabase
+
+class SingleRecipeViewController: UIViewController {
+    
+    // Onload
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //interfaceSetup()
+    }
+    
+    // Hides status bar
+    override var prefersStatusBarHidden: Bool{
+        return true
+    }
+    
+}
