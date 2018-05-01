a8f470f26c9cc4f6f564af374cc23b2b285bbc22
Dylan Rothfeld <drothfel@gmail.com>
2018-05-01 10:32:01 -0700

    added edit recipe view controller storyboard and class

---
+++ Munch/View Controllers/EditRecipeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  EditRecipeViewController.swift
+//  Munch
+//
+//  Created by Dylan Rothfeld on 5/1/18.
+//  Copyright © 2018 Dylan Rothfeld. All rights reserved.
+//
+
+import Foundation
+import FirebaseAuth
+import FirebaseDatabase
+import AVFoundation
+
+class EditRecipeViewController: UIViewController {
+
+}
